006d626cf8d8f03a74ef85853c6cb3f480ca541d
jmarualgit <jairelmarual@gmail.com>
2023-10-11 15:31:27 -0700

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+/*
+ *  Project to learn more about stacks
+ *  
+ *  Stacks is a data structure that follows the LIFO (last in, first out) method.
+ *  If there are three tasks and the middle one is taken away then the last one will
+ *  be on top of the first one; the order stays the same.
+ *  
+ *  Using Java swing graphics, will make a To-Do-List.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
